484493b799f49aac4f48d7d6b3b691c56c73f1dc
elelion <alexander.gints@icloud.com>
2021-08-05 18:24:19 +0300

    Дописали маршруты

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\NewsController as AdminNewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,24 @@ use App\Http\Controllers\MainController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [MainController::class, 'index'])->name('index');
-Route::get('info', [MainController::class, 'info'])->name('info');
-Route::get('news', [MainController::class, 'news'])->name('news');
+
+Route::get('/', [NewsController::class, 'index'])->name('index');
+
+/**
+ * подключаем наши ресурс контроллеры (см. в .../Controllers/Admin/...)
+ *
+ * as => admin - дает дополнительный префикс и на выходе будет запись типа
+ * admin.news.index итп
+ */
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::resource('categories',AdminCategoryController::class);
+    Route::resource('news',AdminNewsController::class);
+});
+
+Route::group(['prefix' => 'news'], function () {
+    Route::get('info', [NewsController::class, 'info'])->name('info');
+    Route::get('news', [NewsController::class, 'news'])->name('news');
+    Route::get('/show/{id?}/{type}', [NewsController::class, 'show'])
+        ->where('id', '\d+')
+        ->name('news.show');
+});
