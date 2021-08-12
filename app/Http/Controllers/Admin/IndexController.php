90d569f13a98a1cd10fc37f1d1aab3288865c46a
elelion <alexander.gints@icloud.com>
2021-08-12 08:27:46 +0300

    создали контроллер одиночку

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Если вы хотите создать контроллер только с одним действием,
+     * вы можете использовать метод __invoke() и даже создать
+     * «вызываемый» (invokable) контроллер.
+     *
+     * Метод __invoke() вызывается, когда скрипт пытается выполнить
+     * объект как функцию.
+     */
+    public function __invoke(Request $request)
+    {
+        return view('admin.index', [
+            'countNews' => count($this->newsList),
+            'countCategories' => 0
+        ]);
+    }
+}
