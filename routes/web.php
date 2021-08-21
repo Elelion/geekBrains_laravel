779f5208d982cb28ad8ca9629d77b6a5fae9eaac
elelion <alexander.gints@icloud.com>
2021-08-21 17:58:56 +0300

    Меняем наши роуты

--- routes/web.php
+++ routes/web.php
@@ -35,12 +35,39 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 Route::group(['prefix' => 'news'], function () {
     Route::get('info', [NewsController::class, 'info'])->name('info');
     Route::get('news', [NewsController::class, 'news'])->name('news');
-    Route::get('/show/{id?}/{type}', [NewsController::class, 'show'])
-        ->where('id', '\d+')
+
+    /**
+     * {news} - аналогично {id} тк laravel сам подставит id из модели news
+     */
+    Route::get('/show/{news?}', [NewsController::class, 'show'])
+        ->where('news', '\d+')
         ->name('news.show');
 });
 
 Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
     Route::get('feedback', [UserController::class, 'feedback'])->name('feedback');
     Route::post('check', [UserController::class, 'check'])->name('check');
+});
+
+Route::get('collection', function () {
+    $collect = collect([
+        ['name' => 'Anna', 'age' => 20, 'work' => 'IT'],
+        ['name' => 'Mike', 'age' => 28, 'work' => 'IT'],
+        ['name' => 'Kate', 'age' => 25, 'work' => 'Education'],
+        ['name' => 'Karl', 'age' => 20, 'work' => 'Marketing'],
+        ['name' => 'Liza', 'age' => 32, 'work' => 'Ads'],
+    ]);
+
+    /**
+     * map - пройти по коллекции и что то сделать
+     * $collect->map(fn($people) => $people['work'])
+     *
+     * max/min - получить мин или мах значения выбранного поля
+     * average - среднее значение
+     * $collect->max('age')
+     *
+     * sortBy - сортировка
+     * $collect->sortBy('age')
+     */
+    dd($collect->sortBy('age'));
 });
